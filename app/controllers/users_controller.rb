9b70e3e20774b9384d1e24f985174e281feedd3c
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-05-29 19:00:59 +0800

    fix signup redirection to dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
         if@user.save
             session[:user_id] = @user.id
             flash[:notice]="Welcome to the Alpha Blog #{@user.username}, you have successfully sign up"
-            redirect_to categories_path
+            redirect_to dashboard_path
         else
             render 'new'
         end
