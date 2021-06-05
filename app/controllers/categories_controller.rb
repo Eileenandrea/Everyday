1a46d7bbab94178ce7ace88e4d26a9441aa6e5d3
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-05 18:35:09 +0800

    additional design and remove require same user on categories page instead get current_user.categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,6 @@
 class CategoriesController < ApplicationController
+    before_action :require_user    
     before_action :set_category, only: [:show,:edit,:update,:destroy]
-    before_action :require_user
-    before_action :require_same_user, only: [:show, :edit, :update, :destroy] 
-    
 
     def index
         @categories = current_user.categories
@@ -43,7 +41,14 @@ class CategoriesController < ApplicationController
     end
     private
     def set_category
-        @category = Category.find(params[:id])
+        categories = current_user.categories
+        if categories.find_by_id(params[:id])
+            @category = categories.find(params[:id])
+        else
+            redirect_back fallback_location: categories_path
+
+        end
+
    end
     def category_params
         params.require(:category).permit(:name,:description,:color)
