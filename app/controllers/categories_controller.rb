82aff4bff92300fcaaa76ac518774c365b12bdee
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-12 22:04:34 +0800

    adding category legend

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,12 +11,25 @@ class CategoriesController < ApplicationController
         @todaytask = current_user.tasks.where('due_date <= ?', DateTime.now.in_time_zone.end_of_day).order(:completed).order(:"#{@sort_by}")
         @todaytask_completed = @todaytask.where(completed:true)
         @todaytask_percent = ((@todaytask_completed.count.to_f / @todaytask.count)*100).round(2)
-    
+        todaytask_color_legend = @todaytask.map{|n| n.category.color}.uniq
+        todaytask_name_legend = @todaytask.map{|n| n.category.name}.uniq
+        @todaytask_legend = []
 
+        todaytask_color_legend.each_with_index do |el,i|
+            @todaytask_legend << {:name => todaytask_name_legend[i], :color => el}        
+        end
+        
+        
         @alltask = current_user.tasks.order(:completed).order(:"#{@sort_by}")
         @alltask_completed = current_user.tasks.where(completed:true)
         @alltask_percent = ((@alltask_completed.count.to_f / @alltask.count)*100).round(2)
-        
+        alltask_color_legend = @alltask.map{|n| n.category.color}.uniq
+        alltask_name_legend = @alltask.map{|n| n.category.name}.uniq
+        @alltask_legend = []
+
+        alltask_color_legend.each_with_index do |el,i|
+            @alltask_legend << {:name => alltask_name_legend[i], :color => el}        
+        end
     end
     def new
         @category = Category.new
